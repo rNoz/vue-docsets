4cc0dfa2a7ecbdb9e8ec868e4522f5985fbbda4e
rNoz <rnoz.commits@gmail.com>
2020-02-08 19:47:48 +0100

    Bugfix: wrong hrefs in Quasar docs. Created issue in their docs.

---
+++ assets/quasar/docs/build/md-plugin-link.js
@@ -0,0 +1,45 @@
+/**
+ * Converts links to DocLink
+ */
+
+module.exports = function (md) {
+  md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
+    const token = tokens[idx]
+
+    const hrefIndex = token.attrIndex('href')
+
+    if (hrefIndex >= 0) {
+      const link = token.attrs[hrefIndex]
+
+      link[0] = 'to'
+      // https://github.com/quasarframework/quasar/issues/6323
+      let href = link[1];
+      const protos = ['http:', 'https:', 'mailto:'];
+      let valid = false;
+      for (let proto of protos){
+        if (href.startsWith(proto)){
+          valid = true;
+          break;
+        }
+      }
+      if (!valid && href[0] !== '/') {
+        if (href.startsWith('../app-extensions')) {
+          href = href.replace(/^\.\.\//, '');
+        }
+        href = `/${href}`;
+      }
+      link[1] = decodeURI(href)
+
+      token.tag = 'doc-link'
+    }
+
+    return self.renderToken(tokens, idx, options)
+  }
+
+  md.renderer.rules.link_close = (tokens, idx, options, env, self) => {
+    const token = tokens[idx]
+
+    token.tag = 'doc-link'
+    return self.renderToken(tokens, idx, options)
+  }
+}
